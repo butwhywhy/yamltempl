31a99f0f9a61d7356bffbfbda35ea9e715d46109
yysa <guillerhr@gmail.com>
2015-01-25 17:01:34 +0100

    Use devtools. setup.py and tox.ini improved

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='yamltempl',
       version='0.1',
+      description='Apply Velocity Template Language to data in YAML format',
       author='Guillermo Horcajada Reales',
       author_email='guillerhr@gmail.com',
-      url='http://www.yyhr.com/yyaml',
+      url='https://github.com/butwhywhy/yamltempl',
+      license='MIT',
       packages=['yamltempl'],
       scripts=['yamltempl/yaml_templates_command.py'],
       package_data={'yamltempl': ['Templates/*']},
+      install_requires=['pyyaml', 'airspeed>=0.4dev'],
       )
